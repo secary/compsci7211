478d617aad729b95e7dd8565e50ad1ec19e5daf7
secary <secary2010@outlook.com>
2024-09-11 14:21:43 +0930

    finalised question16

---
+++ question16.sh
@@ -0,0 +1,8 @@
+#！/bin/bash
+git clone https://github.com/ian-knight-uofa/git-practice-02.git
+cd git-practice-02
+git fetch origin
+git checkout branch3
+git checkout branch2
+git merge branch3
+git branch -d branch3
